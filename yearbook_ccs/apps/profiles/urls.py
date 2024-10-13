573ed11cb7b1eb3dcd862616a4fe74f3c37d7682
Shanley <sebialshanleyflores@gmail.com>
2024-10-13 21:12:28 +0800

    initial batch page

--- yearbook_ccs/apps/profiles/urls.py
+++ yearbook_ccs/apps/profiles/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('profile/', views.profiles_home, name='profile_home'),
     path('setup_profile/', views.setup_profile_2_view, name='setup_profile_2'),
     path('setup_profile/<uidb64>/<token>', views.setup_profile_view, name='setup_profile'),
+    path('batch_page/', views.batch_page_view, name='batch_page'),
 ]
