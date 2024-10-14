5cc663c0464878efc754a97b9abfd26863b21338
jellypedia <angelxo.25@gmail.com>
2024-10-14 21:07:11 +0800

    Blogs should be done. TODO comments model

--- yearbook_ccs/apps/blog/forms.py
+++ yearbook_ccs/apps/blog/forms.py
@@ -1,11 +1,13 @@
 from django import forms
 from .models import Blog
+from django.forms import ClearableFileInput
 
 class BlogForm(forms.ModelForm):
     class Meta:
         model = Blog
-        fields = ('title','content','media')
+        fields = ('title', 'content', 'media')
         widgets = {
-            'title': forms.TextInput(attrs={'placeholder':'Title'}),
-            'content': forms.Textarea(attrs={'placeholder':'Whats on your mind'}),
+            'title': forms.TextInput(attrs={'placeholder': 'Title', 'class': 'form-control m-1'}),
+            'content': forms.Textarea(attrs={'placeholder': 'What\'s on your mind?', 'class': 'form-control m-1'}),
+            'media': ClearableFileInput(attrs={'class': 'form-control m-1'}),
         }
--- yearbook_ccs/apps/blog/urls.py
+++ yearbook_ccs/apps/blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('blog/', views.blog_home, name='blog_home'),
-    path('blog/delete', views.delete_post, name='delete_post'),
+    path('blog/delete/<int:user_id>/', views.delete_post, name='delete_post'),
     path('blog/test', views.intermediary, name='intermediary'),
     path('blog/pending', views.pending_post, name='pending'),
 ]
