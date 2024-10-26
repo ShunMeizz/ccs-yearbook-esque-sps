4a37570ad0b4c4ffcf1f6694dfe0e5f2941d0e65
jellypedia <angelxo.25@gmail.com>
2024-10-26 22:51:26 +0800

    Working edit blog posts

--- yearbook_ccs/apps/blog/views.py
+++ yearbook_ccs/apps/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from .models import Blog, Comment
 
 from .forms import BlogForm, CommentForm
@@ -7,14 +8,36 @@ from .forms import BlogForm, CommentForm
 @login_required
 def blog_home(request):
     posts = get_post()
+    action = request.POST.get('action')
+    print("blog post")
     if request.method == "POST":
         form = BlogForm(request.POST, request.FILES)
-        if form.is_valid():
-            blog = form.save(commit=False)
-            blog.user_id = request.user.id
-            blog.save()
-            # return render(request,"blog/blog_home.html", {'form':form,'blog':blog,'posts':posts})
-            return redirect("intermediary")
+        if action == 'create':
+            if form.is_valid():
+                blog = form.save(commit=False)
+                blog.user_id = request.user.id
+                blog.save()
+                return redirect("intermediary")
+        if action == "edit":
+            post_id = request.POST.get('post_id')
+            blogpost = get_object_or_404(Blog, id=post_id)
+
+            title = request.POST.get('title')
+            content = request.POST.get('content')
+            print("edit")
+
+            blogpost.title = title
+            blogpost.content = content
+            if request.FILES.get('media'):
+                blogpost.media = request.FILES['media']  
+
+            blogpost.save()
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+        if action == "report":
+            post_id = request.POST.get('post_id')
+            blogpost = get_object_or_404(Blog, id=post_id)
+
+            print("report")
     else:
         form = BlogForm() 
     return render(request,"blog/blog_home.html", {'form':form,'posts':posts})
@@ -33,25 +56,45 @@ def comment_section(request):
 def get_post():
     return Blog.objects.all().order_by("-date")
 
-# TO USE FOR FINISHED BLOGPAGE
-# def approved_post(request):
-#     approved = Blog.objects.filter(isApproved=1)
-#     return render(request,"blog/blog_pending.html",{'approved':approved})
-
 def intermediary(request):
     return render(request, "blog/test.html")
 
 def pending_post(request):
-    pending = Blog.objects.filter(isApproved=-1, user=request.user)
-    return render(request,"blog/blog_pending.html",{'pending':pending})
+    user = request.user.username
+    pending = Blog.objects.filter(user=request.user).order_by("-date")
+    action = request.POST.get('action')
+
+    if request.method == "POST":
+        if action == "edit":
+            print("my posts")
+            post_id = request.POST.get('post_id')
+            blogpost = get_object_or_404(Blog, id=post_id)
+
+            title = request.POST.get('title')
+            content = request.POST.get('content')
+
+            blogpost.title = title
+            blogpost.content = content
+            if request.FILES.get('media'):
+                blogpost.media = request.FILES['media']  # Update media if a new file is uploaded
+
+            blogpost.save()
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+        else:
+            return render(request,"blog/blog_pending.html",{'pending':pending,'user':user})
+    else:
+        return render(request,"blog/blog_pending.html",{'pending':pending,'user':user})
+
+def view_post(request, post_id):
+    post = get_object_or_404(Blog, id=post_id)
+    return render(request, 'blog/components/baseblog_post.html', {'post': post})
 
 def delete_post(request, user_id):
     if request.method == "POST":
         post_id = request.POST.get('post_id')
+        action = request.POST.get('action')
         post = get_object_or_404(Blog, id=post_id)
-        if request.user.id == user_id:  
-            post.delete() 
-        else:
-            print("Unauthorized deletion attempt by user:", request.user.id)
 
+        if action == "delete":
+            post.delete()
     return redirect('blog_home')
