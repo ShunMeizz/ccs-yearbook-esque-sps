45cd234008b54ef0616a0a19d5f3b7c945e3f404
jellypedia <angelxo.25@gmail.com>
2024-11-05 16:39:56 +0800

    Prettier edit and homepage edit

--- yearbook_ccs/yearbook_ccs/urls.py
+++ yearbook_ccs/yearbook_ccs/urls.py
@@ -19,8 +19,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.homepage,name="homepage"),
     path('', include('apps.user_management.urls')),
     path('', include('apps.profiles.urls')),
     path('', include('apps.blog.urls')),
