8c9c4cedb154fbb6c18b473be7f14f6fc8b21f00
jellypedia <angelxo.25@gmail.com>
2024-11-06 19:46:03 +0800

    report model added

--- yearbook_ccs/apps/admin_management/models.py
+++ yearbook_ccs/apps/admin_management/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from apps.user_management.models import UserAccount
 
 # Create your models here.
+class Report(models.Model):
+    PENDING_CHOICES = [
+        (0,"Pending"),
+        (1,"Finished"),
+    ]
+
+    # userID, reason, type, status (pending, checked), date, link
+    user_reported = models.ForeignKey(UserAccount, on_delete=models.CASCADE,null=True,blank=True, related_name="user")
+    reason = models.CharField(max_length=200, null=True, blank=True)
+    # to add: report_type: post, comment, profile
+    status = models.IntegerField(choices=PENDING_CHOICES,null=False,default=0)
+    date = models.DateTimeField(auto_now_add=True,null=True, blank=True)
+    link = models.URLField(null=True, blank=True)
