6a206b077ebabbb6bcbb2de9cfaf7688e3ff8ae6
Charlene Repuesto <repuestocharlene@gmail.com>
2024-11-06 18:06:23 +0800

    .

--- yearbook_ccs/yearbook_ccs/views.py
+++ yearbook_ccs/yearbook_ccs/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 import random
 
 from apps.blog.models import Blog
 from apps.profiles.models import UserProfile
 def homepage(request):
+    if (request.user.is_superuser):
+        return redirect('admin_home')
+
     recent_blog = get_recent_blog()
     course = UserProfile.objects.filter(user_account_id=request.user.id).values_list('program',flat=True).first()
     course = "COMPUTER SCIENCE" if course == "BSCS" else "INFORMATION TECHNOLOGY" 
