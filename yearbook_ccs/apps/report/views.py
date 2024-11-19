2e43ed0c39ccae01aa070d4f4581fd917db1a092
jellypedia <angelxo.25@gmail.com>
2024-11-19 12:16:25 +0800

    Blog report and profile report functionality finished

--- yearbook_ccs/apps/report/views.py
+++ yearbook_ccs/apps/report/views.py
@@ -12,31 +12,37 @@ def report_home(request):
     comment_reports = Report.objects.filter(report_type=1).all()
     profile_reports = Report.objects.filter(report_type=2).all()
 
-    form = ReportForm()
-    if request.method == 'POST':
-        print("TEst")
-    
-    return render(request, 'reports.html',{'post_reports':post_reports,
+    return render(request, 'reports_home.html',{'post_reports':post_reports,
                                             'profile_reports':profile_reports,
                                             'comment_reports':comment_reports,
-                                            'username':username,
-                                            'form':form})
+                                            'username':username})
 
 def add_report(request):
     print("POST REPORT")
-    if request.method == "POST" and request.POST.get('action') == 'report':
-        # Create a new report instance
-        report = Report(
-            reason=request.POST.get('report_reason'),
-            report_type=request.POST.get('report_type'),
-            link=request.META.get('HTTP_REFERER'),  # Save the referring page as the link
-            user_reported_id=request.user.id
-        )
-        # Debugging prints
-        print(f"Reason: {report.reason}")
-        print(f"Report Type: {report.report_type}")
-        print(f"Link: {report.link}")
-        report.save()
-        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    if request.POST.get('action') == 'report':
+        if request.method == "POST":
+            report = Report(
+                reason=request.POST.get('report_reason'),
+                report_type=request.POST.get('report_type'),
+                link=request.META.get('HTTP_REFERER'),  # Save the referring page as the link
+                user_reported_id=request.user.id
+            )
+            # Debugging prints
+            print(f"Reason: {report.reason}")
+            print(f"Report Type: {report.report_type}")
+            print(f"Link: {report.link}")
+            report.save()
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
     return render(request, 'reports.html')  # Fallback render if not POST
+
+def finished_report(request):
+    if request.method == 'POST':
+        report_id = request.POST.get('report_id')
+        report = get_object_or_404(Report,pk=report_id)
+        report.status = 1
+        report.save()
+        print('Finished report')
+        print(report_id)
+    
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
